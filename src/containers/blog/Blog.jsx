5b009b5d842676ffbfe0f0b7be6114635aab5068
SesH-iO <sesh.io1022@gmail.com>
2021-11-21 14:58:49 +0530

    Final Build GPT3 OpenAI

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -28,7 +28,7 @@ const blogArts = [
 
 const Blog = () => {
   return (
-    <div className="gpt3__blog section__padding">
+    <div className="gpt3__blog section__padding" id="blog">
       <div className="gpt3__blog-heading">
         <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
       </div>
